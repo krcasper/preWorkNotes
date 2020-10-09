49fc894f077b644a73b748bc66efce25a28d6e0b
Kelly Casper <kellycasper@Kellys-MacBook-Air.local>
2020-10-09 10:29:07 -0400

    first commit

---
+++ JavaScript-Library/JavaScript-Basics/10-practice.js
@@ -0,0 +1,29 @@
+let fruit = {
+
+    name : "Apple",
+
+    price : 1.25
+
+}
+
+console.log(fruit.quantity)
+
+
+let rainbowColors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet'];
+console.log(rainbowColors[5])
+
+let student = {
+
+    name :  "Jack",
+
+    age : 20,
+
+    gpa : 3.5
+
+};
+console.log(student.name);
+
+
+
+var x = 3;
+console.log(x*x);
